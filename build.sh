6087095d4a8c373b80b128ac082962e154112c9c
habijung <habijung0@gmail.com>
2024-07-06 17:43:40 +0900

    Add shell script for project build

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# CMake Generate
+cmake \
+    -S "." \
+    -B "cmake-build" \
+    -G "Visual Studio 17 2022" \
+    -D CMAKE_BUILD_TYPE="Debug"
+
+# CMake Build
+cmake --build "cmake-build"
